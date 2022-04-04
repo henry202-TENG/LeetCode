b7f9786d6bb6255a5d5cce208e34c2c2bc7ef86e
Chihheng.Teng@luxshare-ict.com <chihheng.teng@luxshare-ict.com>
2022-04-04 23:04:35 +0800

    [43.multiply-strings] python3-2

--- 43.multiply-strings.py
+++ 43.multiply-strings.py
@@ -7,7 +7,8 @@
 # @lc code=start
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-
+        # The ord() function returns an integer representing the Unicode character.
+        # The chr() method returns a character (a string) from an integer 
         def str2int(input):
             int1 = 0
             for s in input:
@@ -19,7 +20,7 @@ class Solution:
             if input == 0: return "0"
             s = ""
             while input:
-                s = str( input%10 ) + s
+                s = chr(input%10 + ord("0")) + s
                 input //= 10
             return s
 
