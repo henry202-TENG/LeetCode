7cd74e830c2304b8d5aa9a6be2bda41ed784d704
Chihheng.Teng@luxshare-ict.com <chihheng.teng@luxshare-ict.com>
2022-04-04 21:33:21 +0800

    [55.jump-game] python3-2

--- 55.jump-game.py
+++ 55.jump-game.py
@@ -7,23 +7,14 @@
 # @lc code=start
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        if len(nums) == 1:
-            return True
-
-        index = 0
-        while index < len(nums):
-            Max = 0
-            Max_index = 0
-            for i in range(1, index+2):
-                if Max < nums[index + i]:
-                    Max_index = index + i
-
-            index = index + nums[Max_index]
-
-            if index >= len(nums) - 1:
-                return True
-
-        return False
+        # x is the steps needed to cross the gap.
+        x = 1
+        for i in range(len(nums)-2, -1, -1):
+            if nums[i] < x:
+                x += 1
+            else:
+                x = 1
+        return x == 1
                 
         
 # @lc code=end
