2c2062622b52994977e0b86be25dea60ce607b50
Chihheng.Teng@luxshare-ict.com <chihheng.teng@luxshare-ict.com>
2022-04-05 17:59:38 +0800

    [46.permutations] python3-3

--- 46.permutations.py
+++ 46.permutations.py
@@ -7,19 +7,27 @@
 # @lc code=start
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        # The idea is that 1st ele can swap with ele after 1st ele.
+        # 2ed ele can swap with ele after 2ed ele.
+        # .
+        # .
+        # .
 
-        def trackBack(a, l, r):
-            if l==r:
+        # l is the position we at.
+        def trackBack(a, l):
+            # l is at the end of nums then append.
+            if l==n-1:
                 res.append(a[:])
             else:
-                for i in range(l,r+1):
+                # swap with the following element.
+                for i in range(l,n):
                     a[l], a[i] = a[i], a[l]
-                    trackBack(a, l+1, r)
+                    trackBack(a, l+1)
                     a[i], a[l] = a[l], a[i] # backtrack
 
         n = len(nums)
         res = []
-        trackBack(nums, 0, n-1)
+        trackBack(nums, 0)
 
         return res
  
