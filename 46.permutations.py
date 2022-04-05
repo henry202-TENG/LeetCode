2c05f00b43dd213317e026f0f637b4360b51bd52
Chihheng.Teng@luxshare-ict.com <chihheng.teng@luxshare-ict.com>
2022-04-05 11:55:14 +0800

    [46.permutations] python3-2

--- 46.permutations.py
+++ 46.permutations.py
@@ -8,18 +8,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
 
-        def inner(a, l, r):
+        def trackBack(a, l, r):
             if l==r:
                 res.append(a[:])
             else:
                 for i in range(l,r+1):
                     a[l], a[i] = a[i], a[l]
-                    inner(a, l+1, r)
+                    trackBack(a, l+1, r)
                     a[i], a[l] = a[l], a[i] # backtrack
 
         n = len(nums)
         res = []
-        inner(nums, 0, n-1)
+        trackBack(nums, 0, n-1)
 
         return res
  
