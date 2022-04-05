7496aafba5a850f8ee05ca4764fe0488da827e3c
Chihheng.Teng@luxshare-ict.com <chihheng.teng@luxshare-ict.com>
2022-04-05 23:53:58 +0800

    [23.merge-k-sorted-lists] python3-2

--- 23.merge-k-sorted-lists.py
+++ 23.merge-k-sorted-lists.py
@@ -30,16 +30,15 @@ class Solution:
             n = len(lists)
             for _ in range(n):
                 listNode = lists.pop(0)
-                if listNode:
-                    new_listNode = listNode
-                    while listNode and listNode.val == point_val:
-                        new_listNode = listNode.next # 鍊錶被取走頭之後的新頭
-                        curr.next = listNode         # 被取走的node接到答案後面
-                        listNode.next = None         # 答案最後node指向 None
-                        curr = curr.next             # 從新標定答案鏈尾
-                        listNode = new_listNode      # listNode 指向新錶頭，為了進行下一個循環
-                    if new_listNode:
-                        lists.append(new_listNode)   # 結束則把表放回queue
+                new_listNode = listNode
+                while listNode and listNode.val == point_val:
+                    new_listNode = listNode.next # 鍊錶被取走頭之後的新頭
+                    curr.next = listNode         # 被取走的node接到答案後面
+                    listNode.next = None         # 答案最後node指向 None
+                    curr = curr.next             # 從新標定答案鏈尾
+                    listNode = new_listNode      # listNode 指向新錶頭，為了進行下一個循環
+                if new_listNode:
+                    lists.append(new_listNode)   # 結束則把表放回queue
             point_val += 1
 
         return res.next
