3727fe777a7f725d3d9f0bc749cef8602ea47be6
Chihheng.Teng@luxshare-ict.com <chihheng.teng@luxshare-ict.com>
2022-04-10 16:56:55 +0800

    [169.majority-element] python3

--- 169.majority-element.py
+++ 169.majority-element.py
@@ -7,7 +7,16 @@
 # @lc code=start
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        set_K = set(nums)
+        hash = {}
+        while set_K:
+            hash[set_K.pop()] = 0
+
         for ele in nums:
+            hash[ele] += 1
+            if hash[ele] > n/2:
+                return ele
 
 # @lc code=end
 
