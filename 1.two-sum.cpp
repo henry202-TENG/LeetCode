48251d4242974f21d96be3f3a1d1c0019ecb4a92
Chihheng.Teng@luxshare-ict.com <chihheng.teng@luxshare-ict.com>
2022-05-18 22:34:23 +0800

    [1.two-sum.cpp] v1

---
+++ 1.two-sum.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        map<int,int> hash;
+
+        for (vector<int>::size_type i = 0; i!=nums.size()-1; i++)
+        {
+            for (vector<int>::size_type j = i+1; j!=nums.size(); j++)
+            {
+                if (nums[i]+nums[j] == target)
+                {
+                    res.push_back(i);
+                    res.push_back(j);
+                    return res;
+                }
+            }
+        }
+
+
+        return res;
+    }
+};
+// @lc code=end
+
